9ff14aeda72d8b22b97e981d3a1d306231d3a7f7
avital <avital@pmax.co>
2021-02-25 12:03:05 +0200

    added auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./styles.css";
-
-export default function App() {
+import React from 'react';
+import { withAuthenticator, AmplifySignOut} from "@aws-amplify/ui-react";
+function App() {
   return (
     <div className="App">
+      <header>
+      <h1>We Now have Auth!</h1>
+      </header>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
+
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 import App from "./App";
 
 const rootElement = document.getElementById("root");
